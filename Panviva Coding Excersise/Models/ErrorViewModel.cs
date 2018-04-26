aa20eba5430b3e2938821029decabd2c2eef12e7
Nirav Desai <d08nirav@hotmail.com>
2018-04-26 17:37:01 +1000

    Setup of the site

---
+++ Panviva Coding Excersise/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Panviva_Coding_Excersise.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
