437d191b85108401dc891bbb311cd932da5e5d71
Nirav Desai <d08nirav@hotmail.com>
2018-04-26 20:17:57 +1000

    Connection to GitHub API successfully established.

--- Panviva Coding Excersise/Controllers/HomeController.cs
+++ Panviva Coding Excersise/Controllers/HomeController.cs
@@ -2,19 +2,54 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Panviva_Coding_Excersise.Models;
 
 namespace Panviva_Coding_Excersise.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private static int i = 0;
+        public string Welcome(string name, int numTimes = 1)
         {
-            return View();
+            ViewData["UserName"] = "search required for :" +name;
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
 
+        public async Task<IActionResult> Index(string username)
+        {
+            if (String.IsNullOrEmpty(username))
+            {
+                ViewData["UserName"] = "empty";
+                return View();
+            }
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/search/users?q=" + username);
+                    // Add our custom headers
+                    requestMessage.Headers.Add("User-Agent", "User-Agent-Here");
+                    HttpResponseMessage response = await client.SendAsync(requestMessage);
+                    //client.BaseAddress = new Uri("https://api.github.com/search/users?q=d08nirav");
+                    //var response = await client.GetAsync($"/search/users?q={username}&appid=YOUR_API_KEY_HERE&units=metric");
+                    //var response = await client.GetAsync("https://api.github.com/search/users?q=d08nirav");//.GetAsync($"/search/users?q=d08nirav");
+                    response.EnsureSuccessStatusCode();
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject(stringResult);
+                    ViewData["UserName"] = rawWeather;
+                    return View();
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}" + i);
+                }
+            }
+        }
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
--- Panviva Coding Excersise/Views/Home/Index.cshtml.cs
+++ Panviva Coding Excersise/Views/Home/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Panviva Coding Excersise.Views.Main
+namespace Panviva_Coding_Excersise.Views.Main
 {
     public class IndexModel : PageModel
     {
