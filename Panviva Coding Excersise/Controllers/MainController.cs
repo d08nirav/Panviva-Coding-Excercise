8622630d8c2844881a55a2fc623b96584d0c365f
Nirav Desai <d08nirav@hotmail.com>
2018-04-26 17:37:22 +1000

    setup 2

---
+++ Panviva Coding Excersise/Controllers/MainController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Panviva_Coding_Excersise.Controllers
+{
+    public class MainController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
